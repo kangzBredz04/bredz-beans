49ba57c9274e9db5bff8afa520b273a65fd4c143
Wahyu Jebredz <wabredz1234@gmail.com>
2024-02-22 14:20:14 +0700

    first commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import { connection } from "../db.js";
 
 const app = express();
 app.use(express.json());
@@ -9,40 +8,6 @@ app.get("/api/v1", async (_req, res) => {
   res.send("Hello world!");
 });
 
-// // Get all students
-// app.get("/api/v1/students", async (_req, res) => {
-//   const result = await connection.query("SELECT * FROM students");
-//   res.json(result);
-// });
-
-// // Add student
-// app.post("/api/v1/students", async (req, res) => {
-//   await connection.execute(
-//     `INSERT INTO students VALUES (NULL, '${req.body.name}', ${req.body.generation}, FALSE)`
-//   );
-//   res.send("Mahasiswa berhasil disimpan.");
-// });
-
-// // Get student by ID
-// app.get("/api/v1/students/:id", async (req, res) => {
-//   const result = await connection.query(
-//     `SELECT * FROM students WHERE id = ${req.params.id}`
-//   );
-//   res.json(result[0]);
-// });
-
-// // Edit student by ID
-// app.put("/api/v1/students/:id", async (req, res) => {
-//   await connection.execute(
-//     `UPDATE students SET name = '${req.body.name}', generation = ${req.body.generation}, present = ${req.body.present} WHERE id = ${req.params.id}`
-//   );
-//   res.send("Mahasiswa berhasil diedit.");
-// });
-
-// // Delete student by ID
-// app.delete("/api/v1/students/:id", async (req, res) => {
-//   await connection.execute(`DELETE FROM students WHERE id = ${req.params.id}`);
-//   res.send("Mahasiswa berhasil dihapus.");
-// });
+const router = express.Router();
 
 app.listen(3000, () => console.log("Server berhasil dijalankan."));
